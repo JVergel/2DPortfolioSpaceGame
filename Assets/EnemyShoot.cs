030813b3914ede8d7cc8ca9227d2f512b85cdc2c
JVergel <63523349+JVergel@users.noreply.github.com>
2020-12-17 18:58:14 -0500

    Smart Enemy
    Create an enemy typethat knows when it’sbehind the player, andfires a weaponbackwards.

--- Assets/EnemyShoot.cs
+++ Assets/EnemyShoot.cs
@@ -6,14 +6,38 @@ public class EnemyShoot : MonoBehaviour
 {
     [SerializeField]
     private GameObject Lazer;
+    [SerializeField]
+    private bool SmartOn;
+    private bool CanShoot;
+    private GameObject _player;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Shot());
+        CanShoot = true;
+        if (!SmartOn)
+        {
+            StartCoroutine(Shot());
+        }
+        _player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+    private void Update()
+    {
+        if (SmartOn&& (this.transform.position.y<_player.transform.position.y)&& CanShoot)
+        {
+            Instantiate(Lazer, this.transform.position, Quaternion.identity);
+            StartCoroutine(CanShootSmart());
+        }
     }
 
     // Update is called once per frame
-private IEnumerator Shot()
+    private IEnumerator CanShootSmart()
+    {
+        CanShoot = false;
+        yield return new WaitForSeconds(0.5f);
+        CanShoot = true;
+    }
+    private IEnumerator Shot()
     {
         while (true)
         {
